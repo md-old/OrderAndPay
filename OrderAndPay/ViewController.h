88a75ae1039a5ed9c832f40e6b624f84658270fc
Marco Denisi <marcodenisi@MacBook-Pro-di-Marco.local>
2014-08-23 15:28:11 +0200

    Initial Commit

---
+++ OrderAndPay/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OrderAndPay
+//
+//  Created by Marco Denisi on 23/08/14.
+//  Copyright (c) 2014 Marco Denisi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ OrderAndPayTests/OrderAndPayTests.m
@@ -0,0 +1,34 @@
+//
+//  OrderAndPayTests.m
+//  OrderAndPayTests
+//
+//  Created by Marco Denisi on 23/08/14.
+//  Copyright (c) 2014 Marco Denisi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OrderAndPayTests : XCTestCase
+
+@end
+
+@implementation OrderAndPayTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
